a7a2ed2fbde8430f8ec9c8ff9d55d70fabd3552d
ccremers <ccremers@4f7d145f-a0d4-0310-ae10-d96cc04433c1>
2004-07-16 14:11:19 +0000

    - Added a preliminary SCons script.

--- src/main.c
+++ src/main.c
@@ -70,6 +70,11 @@ const char *progname = "scyther";
  */
 const char *releasetag = "alpha2-devel";
 
+int yywrap (void)
+{
+  return 0;
+}
+
 //! The main body, as called by the environment.
 int
 main (int argc, char **argv)
