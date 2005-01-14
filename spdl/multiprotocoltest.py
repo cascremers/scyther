a3f9d0dc65c4b98c9b8605f4bec80bac4a9ac36b
ccremers <ccremers@4f7d145f-a0d4-0310-ae10-d96cc04433c1>
2005-01-14 09:52:48 +0000

    - Added a few protocols to the list.

--- spdl/multiprotocoltest.py
+++ spdl/multiprotocoltest.py
@@ -36,7 +36,7 @@ ScytherProgram = "../src/scyther"
 
 # Scyther parameters
 ScytherDefaults	= "--summary"
-ScytherMethods	= "--match=1 --arachne"
+ScytherMethods	= "--match=0 --arachne"
 ScytherBounds	= "--timer=300 --max-runs=5 --max-depth=25 --max-length=30"
 
 # Build a large part of the command line (for Scyther) already
