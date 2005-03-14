e73a08a0563de1ebf0ef0e5d3c8c7fc78a1e636e
ccremers <ccremers@4f7d145f-a0d4-0310-ae10-d96cc04433c1>
2005-03-14 09:58:44 +0000

    - Allow for tests on protocols with no known attacks.

--- test/protocollist.py
+++ test/protocollist.py
@@ -9,43 +9,48 @@ def list_ppfix(list, prefix, postfix):
 		newlist.append(prefix + i + postfix)
 	return newlist
 
-def from_literature():
+def from_good_literature():
 	list = [ \
-	"andrew-ban.spdl",
-	"andrew-lowe-ban.spdl",
 	"bke.spdl",
 	"boyd.spdl",
 	"ccitt509-ban.spdl",
-	"denning-sacco-shared.spdl",
 	"gong-nonce-b.spdl",
 	"gong-nonce.spdl",
 	"isoiec11770-2-13.spdl",
-	"kaochow.spdl",
 	"kaochow-v2.spdl",
 	"kaochow-v3.spdl",
-	"ksl.spdl",
-	"ns3.spdl",
 	"nsl3.spdl",
-	"nsl7.spdl",
-	"ns-symmetric-amended.spdl",
-	"ns-symmetric.spdl",
-	"otwayrees.spdl",
-	"soph-keyexch.spdl",
+	"ksl.spdl",
 	"soph.spdl",
 	"splice-as-hc-cj.spdl",
-	"splice-as-hc.spdl",
-	"splice-as.spdl",
-	"tmn.spdl",
-	"wmf-brutus.spdl",
-	"woolam-ce.spdl",
-	"woolam-cmv.spdl",
 	"woolam-pi-f.spdl",
-	"yahalom-ban.spdl",
 	"yahalom-lowe.spdl",
 	"yahalom-paulson.spdl",
 	"yahalom.spdl" ]
 
 	return list_ppfix(list, "../spdl/","")
+
+def from_bad_literature():
+	list = [ \
+	"andrew-ban.spdl",
+	"andrew-lowe-ban.spdl",
+	"denning-sacco-shared.spdl",
+	"kaochow.spdl",
+	"ns3.spdl",
+	"ns-symmetric-amended.spdl",
+	"ns-symmetric.spdl",
+	"otwayrees.spdl",
+	"splice-as-hc.spdl",
+	"splice-as.spdl",
+	"tmn.spdl",
+	"wmf-brutus.spdl",
+	"woolam-cmv.spdl",
+	"yahalom-ban.spdl" ]
+
+	return list_ppfix(list, "../spdl/","")
+
+def from_literature():
+	return from_good_literature() + from_bad_literature()
 
 def from_others():
 	list = [ \
@@ -63,10 +68,13 @@ def from_others():
 	"kaochow-palm.spdl",
 	"ns3-brutus.spdl",
 	"nsl3-nisynch-rep.spdl",
+	"nsl7.spdl",
 	"onetrace.spdl",
 	"samasc-broken.spdl",
 	"simplest.spdl",
+	"soph-keyexch.spdl",
 	"speedtest.spdl",
+	"woolam-ce.spdl",
 	"unknown2.spdl"]
 
 	return list_ppfix(list, "../spdl/","")
@@ -82,6 +90,9 @@ def select(type):
 	elif n == 1:
 		# 1 means from literature
 		return from_literature()
+	elif n == 2:
+		# 2 means from literature, no known attacks
+		return from_good_literature()
 	else:
 		# Otherwise empty list
 		return []
