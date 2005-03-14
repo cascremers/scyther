3e9b4e2c4186bc9484832156660bc49bfe3ec190
ccremers <ccremers@4f7d145f-a0d4-0310-ae10-d96cc04433c1>
2005-03-14 10:25:40 +0000

    - Oops.

--- test/comparebig.sh
+++ test/comparebig.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-./compareheuristics.py --program="../src/scyther" -m0 -b0 -p1
-./compareheuristics.py --program="../src/scyther" -m1 -b0 -p1
-./compareheuristics.py --program="../src/scyther" -m2 -b0 -p1
-./compareheuristics.py --program="../src/scyther" -m0 -b1 -p1
-./compareheuristics.py --program="../src/scyther" -m1 -b1 -p1
-./compareheuristics.py --program="../src/scyther" -m2 -b1 -p1
-./compareheuristics.py --program="../src/scyther" -m0 -b2 -p1
-./compareheuristics.py --program="../src/scyther" -m1 -b2 -p1
-./compareheuristics.py --program="../src/scyther" -m2 -b2 -p1
+./compareheuristics.py --program="../src/scyther" -m0 -b0
+./compareheuristics.py --program="../src/scyther" -m1 -b0
+./compareheuristics.py --program="../src/scyther" -m2 -b0
+./compareheuristics.py --program="../src/scyther" -m0 -b1
+./compareheuristics.py --program="../src/scyther" -m1 -b1
+./compareheuristics.py --program="../src/scyther" -m2 -b1
+./compareheuristics.py --program="../src/scyther" -m0 -b2
+./compareheuristics.py --program="../src/scyther" -m1 -b2
+./compareheuristics.py --program="../src/scyther" -m2 -b2
 
 echo
 echo "Done comparing nearly everything for the heuristics."
