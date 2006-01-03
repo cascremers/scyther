92342683f5def4017574bdd91f1fae0081fea383
ccremers <ccremers@4f7d145f-a0d4-0310-ae10-d96cc04433c1>
2006-01-03 11:34:48 +0000

    - Cleanup of unused structure.

--- src/arachne.h
+++ src/arachne.h
@@ -13,17 +13,4 @@ int isTriviallyKnownAtArachne (const System sys, const Term t, const int run,
 int isTriviallyKnownAfterArachne (const System sys, const Term t,
 				  const int run, const int index);
 
-//! Goal structure
-/**
- * Signals a read event or claim event to which a term has to be bound.
- */
-struct goalstruct
-{
-  int run;			//!< run of goal
-  int index;			//!< index of goal in the run
-  Roledef rd;			//!< pointer to the role definition
-};
-
-typedef struct goalstruct Goal;	//!< pointer to goal structure
-
 #endif
