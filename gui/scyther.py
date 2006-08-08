e6ec1ecd99cfaa152e88f16572f4ef58e52a8e86
ccremers <ccremers@4f7d145f-a0d4-0310-ae10-d96cc04433c1>
2006-08-08 15:57:27 +0000

    - Added python wrapper around the command-line tool.

---
+++ gui/scyther.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# Scyther interface
+#
+
+#---------------------------------------------------------------------------
+
+""" Import externals """
+import sys
+
+#---------------------------------------------------------------------------
+
+""" Import scyther components """
+import Scyther.Scyther as Scyther
+
+#---------------------------------------------------------------------------
+
+def usage():
+    x = Scyther.Scyther()
+    x.xml = False
+    x.options = "--help"
+    x.verify()
+    return x
+
+def simpleRun(args):
+    x = Scyther.Scyther()
+    x.options = args
+    x.verify()
+    return x
+
+if __name__ == '__main__':
+    pars = sys.argv[1:]
+    if len(pars) == 0:
+        print usage()
+    else:
+        print simpleRun(" ".join(pars))
+
+
