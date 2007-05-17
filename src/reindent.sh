9bfa17a464070ce8fe54d2e304d13c61d741c23d
Cas Cremers <cas@scizorli.(none)>
2007-05-18 00:05:34 +0200

    - Added todo.

--- src/reindent.sh
+++ src/reindent.sh
@@ -2,4 +2,6 @@
 # 
 # Indent any changed files, ending in .c or .h
 #
+# TODO: Needs to be rewritten as svn of course is no longer used.
+#
 svn st | grep "^[MA].*\.[ch]$"| awk '{print $2}' | xargs indent
