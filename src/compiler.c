7d584cca1ec7b361ce9aeaef08dc242d047e12b3
Cas Cremers <cas.cremers@gmail.com>
2007-06-11 14:01:04 +0200

    Added GPL 2 License to the C sources.
    A first step towards releasing Scyther completely to the public.

--- src/compiler.c
+++ src/compiler.c
@@ -1,3 +1,22 @@
+/*
+ * Scyther : An automatic verifier for security protocols.
+ * Copyright (C) 2007 Cas Cremers
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
@@ -562,7 +581,7 @@ claimCreate (const System sys, const Protocol protocol, const Role role,
       readvars = compute_read_variables (thisRole);
       while (claimvars != NULL)
 	{
-	  if (!inTermlist(protocol->rolenames, claimvars->term))
+	  if (!inTermlist (protocol->rolenames, claimvars->term))
 	    {
 	      /* only if it is not a role */
 	      if (!inTermlist (readvars, claimvars->term))
