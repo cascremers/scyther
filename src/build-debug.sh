0c06cb7a30c2c384ff6357f027cf5bd477ca3f8c
Cas Cremers <cas.cremers@gmail.com>
2008-05-27 17:21:45 +0200

    Added debug build script.

---
+++ src/build-debug.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Default flags
+CMFLAGS="-D CMAKE_BUILD_TYPE:STRING=Debug"
+
+# Make for linux
+cmake $CMFLAGS                   . && make
+
+echo
+echo
+echo "---------------------------------------------------------"
+echo "Built the Linux binary"
+
+# Copy to the correct location
+cp scyther-linux ../gui/Scyther/
+
+# bonus...
+cp scyther-linux ~/bin
+
+echo Copied the file to the gui directory and \~/bin
+echo "---------------------------------------------------------"
+
