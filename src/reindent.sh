92d2ba9d5ec3e471e3a10700d9e2e8d3c379df07
Cas Cremers <cas.cremers@gmail.com>
2009-03-08 09:29:27 +0100

    Make double reindenting the default to make the process stable.

--- src/reindent.sh
+++ src/reindent.sh
@@ -2,4 +2,8 @@
 # 
 # Indent any files ending in .c or .h
 #
+# Apparently unstable behaviour is possible; a stupid fix for my
+# concrete problem was to always run it twice.
+#
+indent *.c *.h
 indent *.c *.h
