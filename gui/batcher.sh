c2662abaef3948625d0c20ba05593a5b1ac67d75
Cas Cremers <cas.cremers@gmail.com>
2011-03-31 15:01:50 +0200

    More comments.

--- gui/batcher.sh
+++ gui/batcher.sh
@@ -6,6 +6,9 @@
 #
 # Tries to parallellize the jobs, and sends a mail afterwards
 
+echo "================================="
+echo " Phase 0: Setup"
+echo "================================="
 AWKSCRIPT=" { srand(); print int(1000000 * rand()) } "
 RND=`echo | awk "$AWKSCRIPT"`
 
@@ -17,13 +20,27 @@ echo $JOBNAME
 echo $JSONFILE
 echo $BATCHFILE
 
+echo "================================="
+echo " Phase 1: generate jobs list"
+echo "================================="
 bsub -I -N -W 8:00 -J $JOBNAME ./test-mpa.py --pickle $JSONFILE $*
+
+echo "================================="
+echo " Phase 2a: precompute job outputs"
+echo "================================="
 # Each verification has a time limit of 600 seconds = 10 minutes
 # To fit in the one hour queue, that means 5 jobs maximum.
 bsub -I -N -W 8:00 -J $JOBNAME -oo $BATCHFILE ./make-bsub.py $JSONFILE 5 -W 1:00 -J $JOBNAME
 # Due to pending etc. the below may take a while.
 sleep 10
 bash $BATCHFILE
+echo "================================="
+echo " Phase 2b: perfom actual job"
+echo " (after precomputation is done"
+echo "================================="
 bsub -I -N -W 8:00 -J after$JOBNAME -w "ended($JOBNAME)" ./test-mpa.py $*
 
 
+echo "================================="
+echo " Done."
+echo "================================="
