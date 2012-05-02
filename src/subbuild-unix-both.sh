8da5209ce602cb1482e77be49ce85a0b38a84376
Cas Cremers <cas.cremers@gmail.com>
2012-05-02 21:25:31 +0200

    New refactoring of build system for linux, should be clearer.
    We also avoid building the w32 binary by default.

--- src/subbuild-unix-both.sh
+++ src/subbuild-unix-both.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
 
-# Default flags
-CMFLAGS="-D CMAKE_BUILD_TYPE:STRING=Release"
-
-# Make for windows and linux
-cmake $CMFLAGS -D TARGETOS=Win32 . && make
-cmake $CMFLAGS -D TARGETOS=Unix  . && make
+./subbuild-unix-unix.sh
+./subbuild-unix-w32.sh
 
 echo
-echo
-echo "---------------------------------------------------------"
-echo "Built the Linux and Windows binaries"
-
-# Copy to the correct locations
-cp scyther-linux ../gui/Scyther/
-cp scyther-w32.exe ../gui/Scyther/
-
-# bonus...
-cp scyther-linux ~/bin
-
-echo Copied the files to their respective locations and \~/bin
-echo "---------------------------------------------------------"
-
+echo "Built both Linux and w32 versions."
