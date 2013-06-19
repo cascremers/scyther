c6280d745e428e82a0bde0df330fdfd6b9e1f7e3
Cas Cremers <cas.cremers@gmail.com>
2013-06-19 21:50:49 +0200

    Backend DOT graph generation: Removing debugging reporting of 'cost' in attack graphs.
    This was confusing to casual users.

--- src/dotout.c
+++ src/dotout.c
@@ -1660,7 +1660,8 @@ dotSemiState (const System mysys)
   eprintf (", claim type ");
   termPrintRemap (sys->current_claim->type);
   // For debugging:
-  eprintf (", cost %i", computeAttackCost (sys));
+  //eprintf (", cost %i", computeAttackCost (sys));
+
   eprintf ("\";\n");
 
   // Needed for the bindings later on: create graph
