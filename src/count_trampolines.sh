f6942f32605f3919fb8633a30fe57d456704af01
Cas Cremers <cas.cremers@gmail.com>
2018-10-21 23:32:24 +0200

    Fix to temporarily store also intermediate trampoline scan results.

--- src/count_trampolines.sh
+++ src/count_trampolines.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-make clean ; make 2>&1  |grep "warning: trampoline" | sort -u > trampolines.out
-cat trampolines.out
+TMP1="trampolines-raw.out"
+TMP2="trampolines.out"
+
+make clean ; make 2>$TMP1
+cat $TMP1 | grep "warning: trampoline" | sort -u > $TMP2
+cat $TMP2
 echo 
-wc -l trampolines.out
+wc -l $TMP2
 
