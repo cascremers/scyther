d9e17005aedcc38a9e1dec745cf22b594d5c4f43
Cas Cremers <cas.cremers@gmail.com>
2018-10-21 17:29:55 +0200

    Fix duplicate trampoline counting bug in script.

--- src/count_trampolines.sh
+++ src/count_trampolines.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-make clean ; make 2>&1  |grep "warning: trampoline" > trampolines.out
+make clean ; make 2>&1  |grep "warning: trampoline" | sort -u > trampolines.out
 cat trampolines.out
 echo 
 wc -l trampolines.out
