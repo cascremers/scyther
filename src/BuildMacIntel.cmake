f4e9c67c23306f695a46d3389fa596ac570ce917
Cas Cremers <cas.cremers@gmail.com>
2020-02-24 21:36:29 +0100

    Fixing deprecated Mac build settings.

--- src/BuildMacIntel.cmake
+++ src/BuildMacIntel.cmake
@@ -7,5 +7,5 @@
 message (STATUS "Building Apple Mac Intel version")
 set (scythername "scyther-mac")
 add_executable (${scythername} ${Scyther_sources})
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -arch i386 -mmacosx-version-min=10.6")
+set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mmacosx-version-min=10.6")
 
